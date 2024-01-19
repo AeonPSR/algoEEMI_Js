bcc0153c49fba4eb23746893a9aee9730a1003c1
Aeon <floriandevigne1@hotmail.fr>
2024-01-19 06:20:34 -0500

    Sponsored by long titles

---
+++ tabAddThreeNoWaitOnlyTwo.js
@@ -0,0 +1,14 @@
+function AddThreeMinusOne(tab) {
+    tab.push("One")
+    tab.push("Two")
+    tab.push("Three")
+    tab.pop();
+    return (tab);
+}
+
+
+
+
+
+
+console.log(AddThreeMinusOne(["Heya", "Plop", "Bleh"]))
